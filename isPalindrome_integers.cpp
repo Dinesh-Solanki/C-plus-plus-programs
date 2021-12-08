68fe27aadf5a7706c57d596bc23a14d39483ebf4
Dinesh Solanki g <77187635+dinesh-6942@users.noreply.github.com>
2021-12-08 08:18:47 +0530

    Add files via upload

---
+++ isPalindrome_integers.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int reverse_num(int num){
+    int rev = 0;
+    while(num != 0){
+        rev = rev * 10 + num % 10;
+        num /= 10;
+    }
+    return rev;
+}
+
+bool palindrome(int num){
+    return num == reverse_num(num);
+}
+
+
+int main(){
+    int num;
+    cin>>num;
+    cout<<palindrome(num)<<endl;
+    return 0;
+}

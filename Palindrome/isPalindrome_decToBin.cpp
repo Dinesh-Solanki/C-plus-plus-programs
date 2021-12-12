fec94543fa7b343f304491876947269f3dbf7264
Dinesh Solanki g <77187635+dinesh-6942@users.noreply.github.com>
2021-12-12 19:28:26 +0530

    Update isPalindrome_decToBin.cpp

--- Palindrome/isPalindrome_decToBin.cpp
+++ Palindrome/isPalindrome_decToBin.cpp
@@ -1,1 +1,27 @@
-
+bool checkPalindrome(long long N)
+{
+    string s="";
+    while(N>0){
+        int t = N%2;
+        s=s+to_string(t);
+        N=N/2;
+      }
+    string rev = "";
+    for(int i=s.length()-1; i>=0; i--){
+        rev = rev + s[i];  
+    }
+    s=rev;
+    int n=s.length();
+    int start=0;
+    int end=n-1;
+    while(start<=end){
+        if(s[start]==s[end]){
+            start++;
+            end--;
+        }
+        else{
+            return 0;
+        }
+    }
+    return 1;
+}

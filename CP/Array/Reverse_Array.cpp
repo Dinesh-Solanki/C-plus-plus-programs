6e2252fd4cbf31571b333e004a881217459f2a20
Dinesh Solanki g <77187635+dinesh-6942@users.noreply.github.com>
2021-12-14 17:55:27 +0530

    Create Reverse_Array.cpp

---
+++ CP/Array/Reverse_Array.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+void Reverse(int arr[], int n){
+    for(int i=0;i<n/2;i++){
+        swap(arr[i],arr[n-i-1]);
+    }
+}
+
+
+
+void print(int arr[],int n){
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<"\n";
+}
+
+int main() {
+    int arr[]={12,45,23,51,19};
+    int n= sizeof(arr)/sizeof(arr[0]);
+    Reverse(arr,n);
+    cout<<"After Reverse: ";
+    print(arr,n);
+	return 0;
+}
+
+
+
+
+/* Using While Loop
+
+void Reverse(int arr[],int n){
+    int s=0, e=n-1;
+    while(s<=e){
+        swap(arr[s],arr[e]);
+        s++;
+        e--;
+    }
+}
+    }
+    
+}
+*/

07a37c7983e86a834ffa44c365edeb95ed3c2760
Dinesh Solanki g <77187635+dinesh-6942@users.noreply.github.com>
2021-12-14 18:02:39 +0530

    Update Reverse_Array.cpp

--- CP/Array/Reverse_Array.cpp
+++ CP/Array/Reverse_Array.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 using namespace std;
 
-void Reverse(int arr[], int n){
-    for(int i=0;i<n/2;i++){
-        swap(arr[i],arr[n-i-1]);
+void Reverse(int arr[],int n){
+    int s=0, e=n-1;
+    while(s<=e){
+        swap(arr[s],arr[e]);
+        s++;
+        e--;
     }
 }
-
 
 
 void print(int arr[],int n){
@@ -28,7 +30,14 @@ int main() {
 
 
 
-/* Using While Loop
+/* Using for Loop
+
+void Reverse(int arr[], int n){
+    for(int i=0;i<n/2;i++){
+        swap(arr[i],arr[n-i-1]);
+    }
+}
+
 
 void Reverse(int arr[],int n){
     int s=0, e=n-1;
@@ -38,7 +47,7 @@ void Reverse(int arr[],int n){
         e--;
     }
 }
-    }
     
-}
+    
+
 */
